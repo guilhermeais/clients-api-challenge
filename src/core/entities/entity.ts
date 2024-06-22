c642d350d765a23681c9fb146425003b24363a45
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-06-21 22:10:56 -0300

    feat: add base classes for entities and value objects

---
+++ src/core/entities/entity.ts
@@ -0,0 +1,60 @@
+import { UniqueEntityID } from './unique-entity-id';
+
+export type Timestamp = {
+  createdAt: Date;
+  updatedAt?: Date;
+};
+
+export abstract class Entity<Props> {
+  private _id: UniqueEntityID;
+  protected _createdAt: Date;
+  protected _updatedAt?: Date;
+  protected props: Props;
+
+  get id() {
+    return this._id;
+  }
+
+  get createdAt() {
+    return this._createdAt;
+  }
+
+  get updatedAt(): Date | undefined {
+    return this._updatedAt;
+  }
+
+  set updatedAt(date: Date) {
+    this._updatedAt = date;
+  }
+
+  protected constructor(
+    props: Props,
+    id?: UniqueEntityID,
+    timestamp?: Timestamp,
+  ) {
+    this.props = props;
+    this._id = id ?? new UniqueEntityID();
+
+    this._createdAt = timestamp.createdAt ?? new Date();
+    this._updatedAt = timestamp.updatedAt;
+  }
+
+  public equals(entity: Entity<unknown>) {
+    if (entity === this) {
+      return true;
+    }
+
+    if (entity.id.equals(this.id)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  public toProps() {
+    return structuredClone({
+      ...this.props,
+      id: this.id.toString(),
+    });
+  }
+}
---
+++ src/core/entities/unique-entity-id.ts
@@ -0,0 +1,21 @@
+import { uuidv7 } from 'uuidv7';
+
+export class UniqueEntityID {
+  private value: string;
+
+  toString() {
+    return this.value;
+  }
+
+  toValue() {
+    return this.value;
+  }
+
+  constructor(value?: string) {
+    this.value = value ?? uuidv7();
+  }
+
+  public equals(id: UniqueEntityID) {
+    return id.toValue() === this.value;
+  }
+}
