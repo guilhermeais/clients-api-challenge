af2303c4b59deafc402bbe0ddccca20c3f00e15a
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-06-24 08:18:29 -0300

    feat: update AppModule with new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './infra/env/env';
+import { EnvModule } from './infra/env/env.module';
+import { ToolsModule } from './infra/tools/tools.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }),
+    EnvModule,
+    ToolsModule,
+  ],
   controllers: [],
   providers: [],
 })
