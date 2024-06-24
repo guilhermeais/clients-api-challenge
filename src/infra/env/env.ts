d53789a1382d1d29db16e228a1f6378e7f2a4bd3
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-06-24 08:17:55 -0300

    feat: add EnvModule for managing environment variables

---
+++ src/infra/env/env.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  PORT: z.coerce.number().optional().default(3000),
+});
+
+export type Env = z.infer<typeof envSchema>;
