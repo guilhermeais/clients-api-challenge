dcf95a6ba93227e39154b834d2560c3444ff306f
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-06-25 12:33:07 -0300

    feat(CustomerRepository): implements all methods

--- src/infra/database/mongodb/repositories/mongodb-customer-repository.e2e-spec.ts
+++ src/infra/database/mongodb/repositories/mongodb-customer-repository.e2e-spec.ts
@@ -1,10 +1,12 @@
 import { CustomerRepository } from '@/domain/application/gateways/repositories/customer-repository.interface';
+import { Email } from '@/domain/enterprise/entities/value-objects/email';
+import { faker } from '@faker-js/faker';
 import { INestApplication } from '@nestjs/common';
+import { MongoServerError } from 'mongodb';
 import { makeCustomer } from 'test/mocks/domain/enterprise/entities/customer.mock';
 import { makeFakeApp } from 'test/utils/make-fake-app';
 import { DatabaseModule } from '../../database.module';
 import { MongoDbCustomerRepository } from './mongodb-customer-repository';
-import { MongoServerError } from 'mongodb';
 
 describe(`${MongoDbCustomerRepository.name}`, () => {
   let app: INestApplication;
@@ -31,6 +33,20 @@ describe(`${MongoDbCustomerRepository.name}`, () => {
       const customerSaved = await sut.findById(customer.id);
 
       expect(customerSaved).toEqual(customer);
+    });
+
+    it('should update an existing customer', async () => {
+      const customer = makeCustomer();
+
+      await sut.save(customer);
+      customer.name = `${customer.name} updated`;
+
+      await sut.save(customer);
+
+      const customerUpdated = await sut.findById(customer.id);
+
+      expect(customerUpdated).toEqual(customer);
+      expect(customerUpdated.name).toEqual(customer.name);
     });
 
     it('should not save a customer with duplicated email', async () => {
@@ -66,6 +82,107 @@ describe(`${MongoDbCustomerRepository.name}`, () => {
       const customerFound = await sut.findById(customer.id);
 
       expect(customerFound).toBeNull();
+    });
+  });
+
+  describe('existsByEmail()', () => {
+    it('should find a customer by email', async () => {
+      const customer = makeCustomer();
+
+      await sut.save(customer);
+
+      const exists = await sut.existsByEmail(customer.email);
+
+      expect(exists).toEqual(true);
+    });
+
+    it('should return null if customer is not found', async () => {
+      const exists = await sut.existsByEmail(
+        Email.create(faker.internet.email()),
+      );
+
+      expect(exists).toEqual(false);
+    });
+  });
+
+  describe('exists()', () => {
+    it('should find a customer by id', async () => {
+      const customer = makeCustomer();
+
+      await sut.save(customer);
+
+      const exists = await sut.exists(customer.id);
+
+      expect(exists).toEqual(true);
+    });
+
+    it('should return null if customer is not found', async () => {
+      const exists = await sut.exists(makeCustomer().id);
+
+      expect(exists).toEqual(false);
+    });
+  });
+
+  describe('delete()', () => {
+    it('should delete a document', async () => {
+      const customer = makeCustomer();
+
+      await sut.save(customer);
+
+      const exists = await sut.exists(customer.id);
+
+      expect(exists).toEqual(true);
+
+      await sut.delete(customer.id);
+
+      const customerFound = await sut.findById(customer.id);
+
+      expect(customerFound).toBeNull();
+    });
+
+    it('should do nothing if the document does not exists', async () => {
+      await sut.delete(makeCustomer().id);
+    });
+  });
+
+  describe('list()', () => {
+    it('should return empty if there is no customers', async () => {
+      const customers = await sut.list({
+        limit: 10,
+        page: 1,
+      });
+
+      expect(customers.items).toEqual([]);
+      expect(customers.pages).toEqual(0);
+    });
+
+    it('should paginated existing customers', async () => {
+      const costumers = await Promise.all(
+        Array.from({ length: 10 }).map(async () => {
+          const customer = makeCustomer();
+          await sut.save(customer);
+
+          return customer;
+        }),
+      );
+
+      const customers = await sut.list({
+        limit: 5,
+        page: 1,
+      });
+
+      expect(customers.items).toEqual(costumers.slice(0, 5));
+      expect(customers.pages).toEqual(2);
+      expect(customers.total).toEqual(10);
+
+      const secondPage = await sut.list({
+        limit: 5,
+        page: 2,
+      });
+
+      expect(secondPage.items).toEqual(costumers.slice(5, 10));
+      expect(secondPage.pages).toEqual(2);
+      expect(secondPage.total).toEqual(10);
     });
   });
 });
--- src/infra/database/mongodb/repositories/mongodb-customer-repository.ts
+++ src/infra/database/mongodb/repositories/mongodb-customer-repository.ts
@@ -5,16 +5,26 @@ import { Logger } from '@/domain/application/gateways/tools/logger.interface';
 import { Customer } from '@/domain/enterprise/entities/customer';
 import { Email } from '@/domain/enterprise/entities/value-objects/email';
 import { Inject, Injectable } from '@nestjs/common';
-import { MongoClient } from 'mongodb';
+import { Collection, MongoClient, ObjectId } from 'mongodb';
 import { MONGO_DB_CONNECTION } from '../mongodb-connection';
 
+export type CustomerDocument = {
+  _id: ObjectId;
+  name: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
 @Injectable()
 export class MongoDbCustomerRepository implements CustomerRepository {
+  #customerCollection: Collection<CustomerDocument>;
   constructor(
     @Inject(MONGO_DB_CONNECTION)
     private readonly connection: MongoClient,
     private readonly logger: Logger,
-  ) {}
+  ) {
+    this.#customerCollection = this.connection.db().collection('customers');
+  }
 
   async save(customer: Customer): Promise<void> {
     try {
@@ -23,14 +33,22 @@ export class MongoDbCustomerRepository implements CustomerRepository {
         `Saving customer ${customer.name} - ${customer.email.value}...`,
       );
 
-      const customerCollection = this.connection.db().collection('customers');
-
-      await customerCollection.insertOne({
-        _id: customer.id.toValue(),
-        name: customer.name,
-        email: customer.email.value,
-        createdAt: customer.createdAt,
-      });
+      await this.#customerCollection.updateOne(
+        {
+          _id: customer.id.toValue(),
+        },
+        {
+          $set: {
+            _id: customer.id.toValue(),
+            name: customer.name,
+            email: customer.email.value,
+            createdAt: customer.createdAt,
+          },
+        },
+        {
+          upsert: true,
+        },
+      );
 
       this.logger.log(
         MongoDbCustomerRepository.name,
@@ -54,11 +72,20 @@ export class MongoDbCustomerRepository implements CustomerRepository {
         `Finding customer ${id.toString()}...`,
       );
 
-      const customerCollection = this.connection.db().collection('customers');
-
-      const customer = await customerCollection.findOne({
-        _id: id.toValue(),
-      });
+      const customer = await this.#customerCollection.findOne(
+        {
+          _id: id.toValue(),
+        },
+        {
+          projection: {
+            _id: 1,
+            name: 1,
+            email: 1,
+            createdAt: 1,
+            updatedAt: 1,
+          },
+        },
+      );
 
       if (!customer) {
         this.logger.warn(
@@ -96,19 +123,193 @@ export class MongoDbCustomerRepository implements CustomerRepository {
     }
   }
 
-  existsByEmail(email: Email): Promise<boolean> {
-    throw new Error('Method not implemented.');
-  }
-  exists(id: UniqueEntityID): Promise<boolean> {
-    throw new Error('Method not implemented.');
+  async existsByEmail(email: Email): Promise<boolean> {
+    try {
+      this.logger.log(
+        MongoDbCustomerRepository.name,
+        `Checking if customer with email ${email.value} exists...`,
+      );
+
+      const customer = await this.#customerCollection.findOne({
+        email: email.value,
+      });
+
+      if (!customer) {
+        this.logger.warn(
+          MongoDbCustomerRepository.name,
+          `Customer with email ${email.value} not found`,
+        );
+
+        return false;
+      }
+
+      this.logger.log(
+        MongoDbCustomerRepository.name,
+        `Customer with email ${email.value} found`,
+      );
+
+      return true;
+    } catch (error) {
+      this.logger.error(
+        MongoDbCustomerRepository.name,
+        `Error checking if customer with email ${email.value} exists: ${error.message}`,
+        error.stack,
+      );
+
+      throw error;
+    }
   }
 
-  delete(id: UniqueEntityID): Promise<void> {
-    throw new Error('Method not implemented.');
+  async exists(id: UniqueEntityID): Promise<boolean> {
+    try {
+      this.logger.log(
+        MongoDbCustomerRepository.name,
+        `Checking if customer ${id.toString()} exists...`,
+      );
+
+      const customer = await this.#customerCollection.findOne({
+        _id: id.toValue(),
+      });
+
+      if (!customer) {
+        this.logger.warn(
+          MongoDbCustomerRepository.name,
+          `Customer ${id.toString()} not found`,
+        );
+
+        return false;
+      }
+
+      this.logger.log(
+        MongoDbCustomerRepository.name,
+        `Customer ${id.toString()} found`,
+      );
+
+      return true;
+    } catch (error) {
+      this.logger.error(
+        MongoDbCustomerRepository.name,
+        `Error checking if customer ${id.toString()} exists: ${error.message}`,
+        error.stack,
+      );
+
+      throw error;
+    }
   }
-  list(
+
+  async delete(id: UniqueEntityID): Promise<void> {
+    try {
+      this.logger.log(
+        MongoDbCustomerRepository.name,
+        `Deleting customer ${id.toString()}...`,
+      );
+
+      await this.#customerCollection.deleteOne({
+        _id: id.toValue(),
+      });
+
+      this.logger.log(
+        MongoDbCustomerRepository.name,
+        `Customer ${id.toString()} deleted successfully`,
+      );
+    } catch (error) {
+      this.logger.error(
+        MongoDbCustomerRepository.name,
+        `Error deleting customer ${id.toString()}: ${error.message}`,
+        error.stack,
+      );
+
+      throw error;
+    }
+  }
+
+  async list(
     request: PaginatedRequest<Partial<{ name: string; email?: Email }>>,
   ): Promise<PaginatedResponse<Customer>> {
-    throw new Error('Method not implemented.');
+    try {
+      this.logger.log(
+        MongoDbCustomerRepository.name,
+        `Listing customers with ${JSON.stringify(request, null, 2)}...`,
+      );
+
+      const { email, name, limit, page } = request;
+
+      const [result] = await this.#customerCollection
+        .aggregate<{
+          metadata: { total: number };
+          items: CustomerDocument[];
+        }>([
+          {
+            $match: {
+              ...(email && { email: email.value }),
+              ...(name && { name: { $regex: name, $options: 'i' } }),
+            },
+          },
+          { $sort: { _id: 1 } },
+          {
+            $facet: {
+              metadata: [
+                {
+                  $count: 'total',
+                },
+              ],
+              items: [
+                {
+                  $skip: limit * (page - 1),
+                },
+                {
+                  $limit: limit,
+                },
+              ],
+            },
+          },
+          {
+            $project: {
+              metadata: {
+                $arrayElemAt: ['$metadata', 0],
+              },
+              items: 1,
+            },
+          },
+        ])
+        .toArray();
+
+      const { metadata, items } = result;
+
+      this.logger.log(
+        MongoDbCustomerRepository.name,
+        `Customers listed a total of ${metadata?.total} successfully`,
+      );
+
+      const pages = Math.ceil((metadata?.total || 0) / limit);
+
+      return {
+        currentPage: page,
+        items: items.map((item) =>
+          Customer.restore(
+            {
+              name: item.name,
+              email: Email.create(item.email),
+            },
+            new UniqueEntityID(item._id),
+            {
+              createdAt: item.createdAt,
+              updatedAt: item.updatedAt,
+            },
+          ),
+        ),
+        limit,
+        total: metadata?.total || 0,
+        pages,
+      };
+    } catch (error) {
+      this.logger.error(
+        MongoDbCustomerRepository.name,
+        `Error listing customers with ${JSON.stringify(request, null, 2)}: ${error.message}`,
+        error.stack,
+      );
+
+      throw error;
+    }
   }
 }
